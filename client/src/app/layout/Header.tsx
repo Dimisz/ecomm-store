63e3e150eb810426326ea30b938c972baf23b5a6
Vladimir Solovyov <leilyngale@icloud.com>
2023-09-08 10:57:07 +0800

    Scaffold the nav bar

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,5 +1,32 @@
-import { DarkMode, LightMode } from "@mui/icons-material";
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { DarkMode, LightMode, ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, IconButton, List, ListItem, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const midSectionLinks = [
+  { title: 'catalog', path: '/catalog' },
+  { title: 'about', path: '/about' },
+  { title: 'contact', path: '/contact' }
+];
+
+const rightSectionLinks = [
+  { title: 'login', path: '/login' },
+  { title: 'register', path: '/register' },
+];
+
+const renderLinks = (links: { title: string; path: string; }[]) => {
+  return links.map(({ title, path }) => {
+    return(
+      <ListItem
+        component={NavLink}
+        to={path}
+        key={path}
+        sx={{ color: 'inherit', typography: 'h6' }}
+      >
+        {title.toUpperCase()}
+      </ListItem>
+    );
+  });
+}
 
 interface Props {
   darkMode: boolean;
@@ -7,14 +34,22 @@ interface Props {
 }
 
 const Header = ({ darkMode, toggleTheme }: Props) => {
+  const renderedMidSectionLinks = renderLinks(midSectionLinks);
+  const renderedRightSectionLinks = renderLinks(rightSectionLinks);
+
   return(
     <>
       <AppBar position='fixed'>
         <Toolbar>
-          <Typography variant='h6'>
+          <Typography 
+            variant='h6' 
+            component={NavLink} 
+            to='/'
+            sx={{ color: 'inherit', textDecoration: 'none' }}
+          >
             E-KOMM
           </Typography>
-          <IconButton onClick={toggleTheme} sx={{ ml: 'auto' }}>
+          <IconButton size='large' onClick={toggleTheme}>
             { darkMode 
               ? 
               <LightMode titleAccess="Switch to light mode"/> 
@@ -22,6 +57,17 @@ const Header = ({ darkMode, toggleTheme }: Props) => {
               <DarkMode titleAccess="Switch to dark mode"/> 
             }
           </IconButton>
+          <List sx={{display: 'flex'}}>
+           {renderedMidSectionLinks}
+          </List>
+          <IconButton size='large' edge='start' color='inherit' sx={{ mr: 2 }}>
+            <Badge badgeContent='4' color='secondary'>
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+          <List sx={{display: 'flex'}}>
+           {renderedRightSectionLinks}
+          </List>
         </Toolbar>
       </AppBar>
       {/* empty Toolbar below rendered to prevent overlay 
