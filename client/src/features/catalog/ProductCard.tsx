4a11460cb23d2c343c5ca335b29022e97d847665
Vladimir Solovyov <leilyngale@icloud.com>
2023-10-16 17:43:22 +0800

    Fix responsiveness in landscape mode

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+import { Box, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import LoadingButton from '@mui/lab/LoadingButton';
 import { Product } from "../../app/models/product";
 import { Link } from "react-router-dom";
@@ -25,45 +25,49 @@ const ProductCard = ({product}: Props) => {
   }
   return(
     <Card 
-      sx={{ height: {
-          xs: '60vh',
-          md: '70vh',
-        }
+      sx={{
+        height: 500,
+        display: 'flex',
+        flexDirection: 'column',
+        m: 0,
       }}
     >
       <CardMedia
         component='img'
-        height='60%'
-        sx={{ backgroundSize: 'contained', bgcolor: 'primary.light' }}
+        sx={{ backgroundSize: 'contained', bgcolor: 'primary.light', height: '60%'
+        }}
         image={product.pictureUrl}
         title={product.name}
       />
 
-      <CardHeader 
-        title={product.name}
-        titleTypographyProps={{ 
-          sx: {fontWeight: { xs: 400, lg: 600 }, color: 'primary.main' }
-        }}
-      />
-      <CardContent sx={{ pt: 0 }}>
-        <Typography gutterBottom color='secondary' variant="h5">
-          ${product.price.toFixed(2)}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {product.brand} / {product.type}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <LoadingButton 
-            size="small" 
-            onClick={() => handleAddItem(product.id)}
-            loading={loading}
-            loadingIndicator="Adding…" 
-        >
-          Add to cart
-        </LoadingButton>
-        <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
-      </CardActions>
+      
+        <Box height='40%' display='flex' flexDirection='column' justifyContent='space-between' padding={0}>
+          <CardHeader
+            title={product.name}
+            titleTypographyProps={{
+              sx: {fontWeight: { xs: 500, lg: 600 }, fontSize: '1.5rem', color: 'primary.main' }
+            }}
+          />
+          <CardContent sx={{ flexGrow: 1, pt: 0, pb: 0 }}>
+            <Typography gutterBottom color='secondary' variant="h4">
+              ${product.price.toFixed(2)}
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              {product.brand} / {product.type}
+            </Typography>
+          </CardContent>
+          <CardActions sx={{ flexGrow: 1 }}>
+            <LoadingButton
+                size="large"
+                onClick={() => handleAddItem(product.id)}
+                loading={loading}
+                loadingIndicator="Adding…"
+            >
+              Add to cart
+            </LoadingButton>
+            <Button component={Link} to={`/catalog/${product.id}`} size="large">View</Button>
+          </CardActions>
+        </Box>
     </Card>
   );
 }
