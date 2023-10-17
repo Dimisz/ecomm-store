29647d692df79cbca8a71748c8240d57ef6a4bc7
Vladimir Solovyov <leilyngale@icloud.com>
2023-10-17 12:55:07 +0800

    Storing the products in redux store - don't have to call an API every type go to catalog page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -15,8 +15,6 @@ import '@fontsource/roboto/700.css';
 import { Provider } from 'react-redux';
 import { store } from './app/store/configureStore.ts';
 
-
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
