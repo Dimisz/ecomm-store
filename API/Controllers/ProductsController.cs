89cff99381b4709004685e17448173c9cb82a431
Vladimir Solovyov <leilyngale@icloud.com>
2023-10-18 19:38:36 +0800

    Add pagination functionality on the API

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
+using System.Text.Json;
 using API.Data;
 using API.Entities;
 using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,15 +17,17 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy, string searchTerm, string brands, string types)
+        public async Task<ActionResult<List<Product>>> GetProducts([FromQuery] ProductParams productParams)
         {
             var query = _context.Products
-                .Sort(orderBy)
-                .Search(searchTerm)
-                .Filter(brands, types)
+                .Sort(productParams.OrderBy)
+                .Search(productParams.SearchTerm)
+                .Filter(productParams.Brands, productParams.Types)
                 .AsQueryable();
-            return await query.ToListAsync();
+            PagedList<Product> products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
 
+            Response.AddPaginationHeader(products.MetaData);
+            return products;
         }
 
         [HttpGet("{id}")]
@@ -32,6 +36,15 @@ namespace API.Controllers
             Product product = await _context.Products.FindAsync(id);
             if (product == null) return NotFound();
             return Ok(product);
+        }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+            List<string> brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+            List<string> types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+
+            return Ok(new { brands, types });
         }
     }
 }
