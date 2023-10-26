d21cba443bb396f995891e31e192f26664539182
Vladimir Solovyov <leilyngale@icloud.com>
2023-10-26 17:40:13 +0800

    Add a visibility toggle for password field

--- client/src/features/account/Register.tsx
+++ client/src/features/account/Register.tsx
@@ -5,24 +5,48 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { Alert, AlertTitle, List, ListItem, ListItemText, Paper } from '@mui/material';
+import { IconButton, InputAdornment, Paper } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { LoadingButton } from '@mui/lab';
 import agent from '../../app/api/agent';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { useState } from 'react';
 
 
 const Register = () =>  {
-  const [validationErrors, setValidationErrors] = useState([]);
-  
+  //toggle password visibility func
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  // form validators and controls
   const { 
     register, 
     handleSubmit, 
+    setError,
     formState: {isSubmitting, errors, isValid} } = useForm({
       mode: 'onTouched'
     });
   
+    const handleApiErrors = (errors: any) => {
+      if(errors){
+        errors.forEach((error: string) => {
+          if(error.includes('Password')){
+            setError('password', { message: error });
+          }
+          else if(error.includes('Email')){
+            setError('email', { message: error });
+          }
+          else if(error.includes('Username')){
+            setError('username', { message: error });
+          }
+        })
+      }
+    }
+
 
   return (
     // <ThemeProvider theme={defaultTheme}>
@@ -47,7 +71,7 @@ const Register = () =>  {
             component="form" 
             onSubmit={
               handleSubmit((data) => agent.Account.register(data)
-              .catch((error) => setValidationErrors(error)))
+              .catch((error) => handleApiErrors(error)))
             } 
             noValidate sx={{ mt: 1 }}>
             <TextField
@@ -67,7 +91,15 @@ const Register = () =>  {
               fullWidth
               label="Email"
               autoComplete="email"
-              {...register('email', {required: 'Email is required'})}
+              {...register('email', 
+                {
+                  required: 'Email is required',
+                  pattern: {
+                    value: /^\w+[\w-\.]*\@\w+((-\w+)|(\w*)).[a-z]{2,3}$/,
+                    message: 'Not a valid email address'
+                  }
+                })
+              }
               error={!!errors.email}
               helperText={errors?.email?.message as string}
             />
@@ -76,32 +108,35 @@ const Register = () =>  {
               required
               fullWidth
               label="Password"
-              type="password"
+              type={showPassword ? "text" : "password"}
               autoComplete="current-password"
-              {...register('password', {required: 'Password is required'})}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              {...register('password', 
+                {
+                  required: 'Password is required',
+                  pattern: {
+                    value: /(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$/,
+                    message: 'Password does not meet complexity requirements'
+                  }
+                })
+              }
               error={!!errors.password}
               helperText={errors?.password?.message as string}
-            />
-
-          {
-            validationErrors.length > 0 && 
-            <Alert severity='error'>
-              <AlertTitle>Validation Errors</AlertTitle>
-              <List>
-                {
-                  validationErrors.map((error) => {
-                    return(
-                      <ListItem key={error}>
-                        <ListItemText>{error}</ListItemText>
-                      </ListItem>
-                    );
-                  })
-                }
-              </List>
-            </Alert>
-          }
-
-
+            >
+            </TextField>
             <LoadingButton
               disabled={!isValid}
               loading={isSubmitting}
